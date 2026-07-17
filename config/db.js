6c78f46cb79a9c708be1d789b6f79adfd1a342cb
GauravSaini104 <gaurav-saini@pearlorganisation.com>
2026-07-17 12:44:13 +0530

    ok

--- config/db.js
+++ config/db.js
@@ -2,17 +2,22 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-
         await mongoose.connect(process.env.MONGODB_URI);
-
         console.log("✅ MongoDB Connected");
 
+        const db = mongoose.connection.db;
+        const collections = await db.listCollections({ name: 'users' }).toArray();
+        if (collections.length > 0) {
+            try {
+                await db.collection('users').dropIndex('email_1');
+                console.log("🗑️ Old index dropped");
+            } catch (err) {
+                
+            }
+        }
     } catch (error) {
-
         console.log(error.message);
-
         process.exit(1);
-
     }
 };
 
