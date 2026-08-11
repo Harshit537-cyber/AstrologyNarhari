13845e6fbc8a9afe715b291876ef5722bb51208d
Harshit <harshit537cyber@gmail.com>
2026-08-11 15:02:16 +0530

    ok

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use("/api/agora", require("./routes/agora/agoraRoutes"));
 app.use("/api/admin/card", require("./routes/AdminRoutes/cardRoutes"));
 app.use("/api/admin/shop/banner", require("./routes/AdminRoutes/E-comm/topBannerRoutes"));
 app.use("/api/partner/rating", require("./routes/PatnerRoutes/partnerRatingRoutes"));
-app.use("/api/partner/pandit", require("./routes/AdminRoutes/panditJiRoutes"));
+app.use("/api/admin/pandit", require("./routes/AdminRoutes/panditJiRoutes"));
 
 app.get("/", (req, res) => {
     res.status(200).json({
