024f97f60c79e5c0e1065fd79747873578f9a2d8
GauravSaini104 <gaurav-saini@pearlorganisation.com>
2026-09-12 12:44:51 +0530

    ok

--- models/Transaction/Transaction.js
+++ models/Transaction/Transaction.js
@@ -7,8 +7,8 @@ const transactionSchema = new mongoose.Schema({
     amount: { type: Number, required: true },
     currency: { type: String, default: 'INR' },
     status: { type: String, enum: ['pending', 'success', 'failed'], default: 'pending' },
-    type: { type: String, enum: ['deposit', 'booking_fee', 'refund', 'payout', 'debit'], default: 'deposit' }, // 'debit' add kar diya
-    description: { type: String } 
+    type: { type: String, enum: ['deposit', 'booking_fee', 'refund', 'payout', 'debit'], default: 'deposit' },
+    description: { type: String }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Transaction', transactionSchema);
